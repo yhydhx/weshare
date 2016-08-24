d0c78de7ac5155e334fc778e7c13d131f800b909
LumiaXu <xr19960722@gmail.com>
2016-08-24 12:56:51 +0800

    debug

--- frontEnd/views.py
+++ frontEnd/views.py
@@ -26,10 +26,12 @@ import time
 import datetime
 from urllib import urlencode, unquote
 import urllib2
+import logging
 from gt.settings import SALT, TENCENT_APPID, TENCENT_APPKEY
 
 
 def index(request):
+    log = logging.getLogger('viewloggingbugs')
     provinces = Province.objects.all()
     d = {}
     for s_province in provinces:
@@ -95,19 +97,17 @@ def index(request):
             open_id = urlencode2dict(ret_open_id.split(' '))['openid']
 
             #### for test ####
-            f = open('test_v.txt', 'w')
-            f.write('return code get, and handle request\n')
-            f.write('code: ' + code + '\n')
-            f.write('qdict: ' + str(qdict) + '\n')
-            f.write('address: ' + address + '\n')
-            f.write('ret_qq_token: ' + ret_qq_token + '\n')
-            f.write('ret_token: ' + ret_token + '\n')
-            f.write('access_token: ' + access_token + '\n')
-            f.write('access_token_dict: ' + str(access_token_dict) + '\n')
-            f.write('address_2: ' + address_2 + '\n')
-            f.write('ret_open_id: ' + ret_open_id + '\n')
-            f.write('ret_open_id ' + ret_open_id + '\n')
-            f.close()
+            log.debug('return code get, and handle request\n')
+            log.debug('code: %s\n', code)
+            log.debug('qdict: %s\n', str(qdict))
+            log.debug('address: %s\n', address)
+            log.debug('ret_qq_token: %s\n', ret_qq_token)
+            log.debug('ret_token: %s\n', ret_token)
+            log.debug('access_token: %s\n', access_token)
+            log.debug('access_token_dict: %s\n', str(access_token_dict))
+            log.debug('address_2: %s\n', address_2)
+            log.debug('ret_open_id: %s\n', ret_open_id)
+            log.debug('ret_open_id: %s\n', ret_open_id)
 
             ####
             try:
@@ -131,9 +131,7 @@ def index(request):
             return render_to_response('frontEnd/account.html', Info)
 
         except:
-            f = open('test_v.txt', 'w')
-            f.write('no code get and did not handle the code')
-            f.close()
+            log.debug('no code get and did not handle the code')
             return render_to_response('frontEnd/index.html', Info)
 
 
