3cf563e483ce41f2f76eb8865be2b2df5d94d4cb
jupiter <yhydhx@gmail.com>
2016-09-20 13:37:46 +0800

    fix bugs

--- gt/models.py
+++ gt/models.py
@@ -683,7 +683,7 @@ class Host_Topic(models.Model):
         for host_topic_atom in host_topics:
             if not host_dict.has_key(host_topic_atom.host_id):
                 host_dict[host_topic_atom.host_id] = 1
-                host_atom = Host.objects.get(id=host_id).format_dict()
+                host_atom = Host.objects.get(id=host_topic_atom.host_id).format_dict()
                 result.append(host_atom)
         return result
 
