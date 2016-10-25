5fa4b1188bfc049861597c53219121c415fa57ea
LumiaXu <xr19960722@gmail.com>
2016-10-25 13:39:49 +0800

    fix

--- frontEnd/views.py
+++ frontEnd/views.py
@@ -477,14 +477,14 @@ def ichange(request):
             if new_password != new_password_confirm or process_passwd(new_password):
                 PASSWORD_CONFIRM_ERROR = True
                 return render_to_response('frontEnd/ichange.html', {'current_user': user,
-                                                                    'change_mark': True,
+                                                                    'change_mark': False,
                                                                     'PASSWORDCONFIRMERROR': PASSWORD_CONFIRM_ERROR})
             else:
                 user.password = new_password
                 user.save()
                 del request.session
                 return render_to_response('frontEnd/ichange.html', {'current_user': user,
-                                                                    'change_mark': False},
+                                                                    'change_mark': True},
                                           context_instance=RequestContext(request))
     else:
         return render_to_response('frontEnd/ichange.html', {'current_user': user})
